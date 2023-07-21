ced430fa97b50960c051c87fdc8d57f8babc715c
mahdi9b <mahdi9booo@gmail.com>
2023-07-21 12:25:22 +0330

    Mobile

--- mobil_user.js
+++ mobil_user.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+
+
+
+    const btn = document.getElementById("btn")
+    btn.addEventListener("click",()=>{
+      document.querySelector(".Add_user_btn").classList.add("show")
+      document.querySelector(".back_user_btn").classList.remove("dis-none")
+    })
+
+
+
+    document.querySelector(".back_user_btn").addEventListener("click",()=>{
+      document.querySelector(".Add_user_btn").classList.remove("show")
+      document.querySelector(".back_user_btn").classList.add("dis-none")
+    })
+
+ 
+
   });
