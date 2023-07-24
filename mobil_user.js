9c0d083153b2bbc0dc3ad7d35e2d296494c854b0
mahdi9b <mahdi9booo@gmail.com>
2023-07-24 09:07:59 +0330

    first commit

--- mobil_user.js
+++ mobil_user.js
@@ -21,3 +21,36 @@ document.addEventListener('DOMContentLoaded', function() {
  
 
   });
+
+  setTimeout(function(){
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((json) => { 
+      console.log(json)
+  
+
+      let html = ""
+
+
+      for (const user  of json){
+        html=html+`<tr>
+                    <td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.company.city}</td>
+                    <td>${user.address.city}</td>
+                    <td>
+                      <i class="material-icons red-text">delete_forever</i>
+                      <i class="material-icons yellow-text">create</i>
+                    </td>
+                  </tr>`
+
+      }
+
+      document.querySelector(".mamad").innerHTML = html
+    })
+
+
+
+  },0)
